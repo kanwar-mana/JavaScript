f8b5955d1fa2c1953a122e228111ff8b4f96974b
kanwar-mana <kanwarabdullrahman@gmail.com>
2024-01-28 17:01:04 +0500

    Initial commit

---
+++ Lecture 1/hellome.js
@@ -0,0 +1,60 @@
+console.log("Hello Kanwer Abdull Rahman");
+console.log("It's me first time writing javascript code");
+console.log("It's University of Engineering and Technology, Taxila Baby dont take it polite! Yes");
+
+name = "Kanwer Abdull Rahman";
+radius = 457;
+percentage = 45.99;
+console.log(percentage);
+console.log(name);
+console.log(radius);
+console.log(percentage);
+
+///////*variable rules*//////
+
+analyserAli="kanwer";
+AnalyserAli="Ali";
+console.log(analyserAli+AnalyserAli);
+
+$ali12=45;
+console.log($ali12);
+
+let sust="kanwer";
+let id="nacromancer";
+let phone="03054418833";
+console.log(sust,id,phone);
+
+var kali=12;
+var kali=23;
+var kali=45;
+console.log(kali);
+
+let baby=78;
+baby=34;
+console.log(baby);
+
+const kareem=67;
+console.log(kareem);
+
+let hassi;
+console.log(hassi);
+
+const student = {
+    fullName:"Devworld",
+    age:20,
+    cgpa:3.2,
+    isPass:true,
+};
+student["age"]=student["age"]+10;
+console.log(student["age"]);
+console.log(student.isPass);
+
+const pakistan= {
+    punjab : 2500,
+    sindh:300,
+    blochistan : 2300,
+};
+
+console.log(pakistan);
+pakistan["punjab"]=pakistan["punjab"]+20;
+console.log(pakistan["punjab"]);
---
+++ Lecture 2/oper-condition.js
@@ -0,0 +1,20 @@
+//Welcome to second javascript lecture
+//Operators
+// let a=4;
+// let b=2;
+// let c=5;
+
+// console.log("a + b= ",a+b);
+// console.log("a - b= ",a-b);
+// console.log("a * c= ",a*c);
+// console.log("b / a= ",b/a);
+// console.log("c % a=",c%a);     //Modulus
+// console.log("a ** b= ",a**b);  //Exponentiation
+
+let a=5;
+let b=4;
+console.log("a =",a,"& b =",b)
+a=a+1;
+console.log("a =",a);
+b=b-1;
+console.log("b =",b);
